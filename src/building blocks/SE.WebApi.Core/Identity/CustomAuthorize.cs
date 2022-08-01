41b3d29081d0e760b3a95b262804132bf297cd6d
Matheus Tessaro <matheusnossabein@positivosolucoes.com.br>
2022-08-01 19:38:58 -0300

    Added custom authorization class for user claims validation

---
+++ src/building blocks/SE.WebApi.Core/Identity/CustomAuthorize.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+using System.Security.Claims;
+
+namespace SE.WebApi.Core.Identity
+{
+    public class CustomAuthorize
+    {
+        public static bool ValidateUserClaims(HttpContext context, string claimName, string claimValue) =>
+            context.User.Identity.IsAuthenticated &&
+            context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+    }
+
+    public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(ClaimFilterRequest)) =>
+            Arguments = new object[] { new Claim(claimName, claimValue) };
+    }
+
+    public class ClaimFilterRequest : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+        public ClaimFilterRequest(Claim claim) => _claim = claim;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new StatusCodeResult(401);
+                return;
+            }
+
+            if (!CustomAuthorize.ValidateUserClaims(context.HttpContext, _claim.Type, _claim.Value))
+                context.Result = new StatusCodeResult(403);
+        }
+    }
+}
--- src/services/SE.Catalog.API/Controllers/CatalogController.cs
+++ src/services/SE.Catalog.API/Controllers/CatalogController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SE.Catalog.API.Models;
+using SE.WebApi.Core.Identity;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SE.Catalog.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/catalog")]
     public class CatalogController : ControllerBase
@@ -13,10 +16,12 @@ namespace SE.Catalog.API.Controllers
         private readonly IProductRepository _productRepository;
         public CatalogController(IProductRepository productRepository) => _productRepository = productRepository;
 
+        [AllowAnonymous]
         [HttpGet("products")]
         public async Task<IEnumerable<Product>> GetProducts() =>
             await _productRepository.GetAll();
 
+        [ClaimsAuthorize("Catalog", "Read")]
         [HttpGet("products/{id}")]
         public async Task<Product> GetProductById([FromRoute] Guid id) =>
             await _productRepository.GetById(id);
