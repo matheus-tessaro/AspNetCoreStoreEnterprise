4b76e8b14adfaa72aa72d19cdcd3ae47e434f51b
Matheus Tessaro <matheusnossabein@positivosolucoes.com.br>
2022-07-31 22:11:42 -0300

    Moved the function that returned the StringContent to the Service class and created a function that accepts a generic parameter to deserialize the JSON response object

--- src/web/mvc/SE.WebApp.MVC/Services/IdentityService.cs
+++ src/web/mvc/SE.WebApp.MVC/Services/IdentityService.cs
@@ -1,9 +1,6 @@
 ﻿using SE.WebApp.MVC.Models;
 using SE.WebApp.MVC.Models.Internal;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SE.WebApp.MVC.Services
@@ -11,32 +8,18 @@ namespace SE.WebApp.MVC.Services
     public class IdentityService : Service, IIdentityService
     {
         private readonly HttpClient _httpClient;
-        private readonly JsonSerializerOptions _jsonSerializerOptions;
-
-        public IdentityService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-            _jsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        }
+        public IdentityService(HttpClient httpClient) => _httpClient = httpClient;
 
         public async Task<UserLoginResponse> Authentication(UserAuthenticationViewModel model)
         {
-            StringContent content = GetStringContent(model);
+            StringContent content = GetContent(model);
             return await PostRequest(content, "https://localhost:44325/api/identity/authentication");
         }
 
         public async Task<UserLoginResponse> Register(UserRegistryViewModel model)
         {
-            StringContent content = GetStringContent(model);
+            StringContent content = GetContent(model);
             return await PostRequest(content, "https://localhost:44325/api/identity/register");
-        }
-
-        private static StringContent GetStringContent(dynamic model)
-        {
-            StringContent content = new(JsonSerializer.Serialize(model), Encoding.UTF8, "appliation/json");
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-            return content;
         }
 
         private async Task<UserLoginResponse> PostRequest(StringContent content, string requestUri)
@@ -44,9 +27,9 @@ namespace SE.WebApp.MVC.Services
             HttpResponseMessage response = await _httpClient.PostAsync(requestUri, content);
 
             if (!HandleResponseErrors(response))
-                return new UserLoginResponse { ResponseResult = JsonSerializer.Deserialize<ResponseResult>(await response?.Content.ReadAsStringAsync(), _jsonSerializerOptions) };
+                return new UserLoginResponse { ResponseResult = await DeserializeResponseObject<ResponseResult>(response) };
 
-            return JsonSerializer.Deserialize<UserLoginResponse>(await response?.Content.ReadAsStringAsync(), _jsonSerializerOptions);
+            return await DeserializeResponseObject<UserLoginResponse>(response);
         }
     }
 }
--- src/web/mvc/SE.WebApp.MVC/Services/Service.cs
+++ src/web/mvc/SE.WebApp.MVC/Services/Service.cs
@@ -1,11 +1,26 @@
 ﻿using SE.WebApp.MVC.Extensions;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace SE.WebApp.MVC.Services
 {
     public abstract class Service
     {
+        protected StringContent GetContent(object data)
+        {
+            StringContent content = new(JsonSerializer.Serialize(data), Encoding.UTF8, "appliation/json");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return content;
+        }
+
+        protected async Task<T> DeserializeResponseObject<T>(HttpResponseMessage responseMessage) =>
+            JsonSerializer.Deserialize<T>(await responseMessage?.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
         protected bool HandleResponseErrors(HttpResponseMessage response)
         {
             switch (response.StatusCode)
