17c99ecc23e4136e3f7ad91838f1eb1bda8a1af9
Matheus Tessaro <matheusnossabein@positivosolucoes.com.br>
2022-08-01 12:24:28 -0300

    Changed frontend of the mvc client for future catalog API implementations

---
+++ src/web/mvc/SE.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace SE.WebApp.MVC.Extensions
+{
+    public static class RazorHelpers
+    {
+        public static string HashEmailForGravatar(this RazorPage page, string email)
+        {
+            var md5Hasher = MD5.Create();
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+            var sBuilder = new StringBuilder();
+
+            foreach (var t in data)
+                sBuilder.Append(t.ToString("x2"));
+
+            return sBuilder.ToString();
+        }
+
+        public static string FormatCurrency(this RazorPage page, decimal value) =>
+            value > 0 ? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", value) : "Free";
+
+        public static string StockMessage(this RazorPage page, int quantity) =>
+            quantity > 0 ? $"Only {quantity} left in stock!" : "Out of stock!";
+    }
+}
