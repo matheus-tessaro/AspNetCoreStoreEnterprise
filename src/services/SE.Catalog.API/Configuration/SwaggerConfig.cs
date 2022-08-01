4843323c5b660118c3a286f508dbeee6530ad710
Matheus Tessaro <matheusnossabein@positivosolucoes.com.br>
2022-08-01 19:38:01 -0300

    Added bearer security scheme to the Catalog API swagger

--- src/services/SE.Catalog.API/Configuration/SwaggerConfig.cs
+++ src/services/SE.Catalog.API/Configuration/SwaggerConfig.cs
@@ -12,22 +12,50 @@ namespace SE.Catalog.API.Configuration
         public static void AddSwaggerConfiguration(this IServiceCollection services) =>
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "StoreEnterprise Catalog API",
-                    Description = "Product/catalog handling",
-                    Contact = new OpenApiContact
+                c.SwaggerDoc("v1", 
+                    new OpenApiInfo
                     {
-                        Name = "Matheus Tessaro",
-                        Email = "notmyrealemail@outlook.com"
-                    },
-                    License = new OpenApiLicense
+                        Title = "StoreEnterprise Catalog API",
+                        Description = "Product/catalog handling",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Matheus Tessaro",
+                            Email = "notmyrealemail@outlook.com"
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "MIT",
+                            Url = new Uri("https://opensource.org/licenses/MIT")
+                        },
+                        Version = "v1"
+                    });
+
+                c.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
                     {
-                        Name = "MIT",
-                        Url = new Uri("https://opensource.org/licenses/MIT")
-                    },
-                    Version = "v1"
-                });
+                        Name = "Authorization",
+                        Description = "Inform the JWT bearer like following the example: Bearer {jwt}",
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey
+                    });
+
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Id = "Bearer",
+                                    Type = ReferenceType.SecurityScheme
+                                }
+                            },
+                            Array.Empty<string>()
+                        }
+                    });
             });
 
         public static void UseSwaggerConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
